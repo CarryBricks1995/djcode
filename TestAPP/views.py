77341a5716877d20ac8893e2d0ee8dd2757863fd
CarryBricks1995 <18807459987@163.com>
2019-07-17 14:09:04 +0800

    测试github提交代码，更改/views.py中的函数hello函数

--- TestAPP/views.py
+++ TestAPP/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def hello(request):
-    context = {'hello'}
+    context = {'hello, world!'}
     return render(request, 'login.html',context)
 
 def book_details(request):
